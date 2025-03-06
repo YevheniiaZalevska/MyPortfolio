d8069f6558a8223ba32f17c9e9ff37e4e36e67d4
YevheniiaZalevska <yevhenia05@gmail.com>
2025-03-06 15:14:13 +0100

    Update main.js

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './js/modal.js';
+import './js/header.js';
+import './js/my-projects.js';
+import './js/faq.js';
+import './js/reviews.js';
+import './js/work-together.js';
+import './js/tech-skills.js';
+import './js/switcher.js';
